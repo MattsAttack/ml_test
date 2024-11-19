36de571aa0bc962696465d56bb6bb479f7b6bb4d
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-11-18 21:01:04 -0600

    camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ml_test/views/camera_view.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: CameraView(),
         ),
       ),
     );
